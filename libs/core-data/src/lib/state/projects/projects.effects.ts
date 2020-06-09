cc95c009d40e94af0c522633bab6133c4d060f2d
https://bpfsoftwaresolutions@dev.azure.com <humberto.mori@proximitycr.com>
2020-06-08 23:21:49 -0500

    v9, how to handle server calls with nx data persistance of ngrx

--- libs/core-data/src/lib/state/projects/projects.effects.ts
+++ libs/core-data/src/lib/state/projects/projects.effects.ts
@@ -2,6 +2,10 @@ import {Injectable} from "@angular/core";
 import { Actions,
          Effect,
          ofType} from "@ngrx/effects";
+import { DataPersistence } from "@nrwl/nx";
+import { switchMap,
+  tap,
+  map } from "rxjs/operators";
 import { ProjectsService } from "../../projects/projects.service";
 import { ProjectsActionTypes,
          LoadProjects,
@@ -12,49 +16,68 @@ import { ProjectsActionTypes,
          ProjectDeleted,
          DeleteProject,
          UpdateProject} from "./projects.actions";
-import { switchMap,
-         map } from "rxjs/operators";
 import { Project } from "../../projects/project.model";
+import { ProjectsState } from "./projects.reducer";
+
 
 @Injectable({providedIn: 'root'})
 export class ProjectsEffects {
 
-  @Effect() loadProjects$ = this.actions$.pipe(
-    ofType(ProjectsActionTypes.LoadProjects),//trigger event
-    switchMap((action : LoadProjects) =>
-      this.projectsService.all()
-        .pipe(
-          map((res: Project[]) => new ProjectsLoaded(res)) // completion event
-        )
-    )
-  );
+  @Effect() loadProjects$ =
+       this.dataPersistence
+        .fetch(ProjectsActionTypes.LoadProjects,
+               { run: (action: LoadProjects, state: ProjectsState) => {
+                        return this.projectsService.all()
+                                  .pipe(map( (res: Project[]) => new ProjectsLoaded(res) )   );
+                      },
+                 onError: ( action: LoadProjects, error) => {
+                   console.log('ERROR',error);
+                 }
+               }
+          );
 
-  @Effect() addProject$ = this.actions$.pipe(
-    ofType(ProjectsActionTypes.AddProject), //trigger event
-    switchMap( (action: AddProject) =>
-      this.projectsService.create(action.payload)
-        .pipe(map((res: Project) => new ProjectAdded(res)))//completion event
-    )
-  );
+  @Effect() addProject$ =
+    this.dataPersistence
+        .pessimisticUpdate(ProjectsActionTypes.AddProject,
+              { run: ( action: AddProject, state: ProjectsState) => {
+                      return this.projectsService.create(action.payload)
+                          .pipe(map((res: Project) => new ProjectAdded(res)));
+                    },
+                onError: (action: AddProject, error) => {
+                  console.log('ERROR',error);
+                }
+              }
+          );
 
-  @Effect() update$ = this.actions$.pipe(
-    ofType(ProjectsActionTypes.UpdateProject), //trigger event
-    switchMap( (action: UpdateProject) =>
-      this.projectsService.update(action.payload)
-        .pipe(map((res: Project) => new ProjectUpdated(res)))//completion event
-    )
-  );
+  @Effect() update$ =
+    this.dataPersistence
+    .pessimisticUpdate(ProjectsActionTypes.UpdateProject,
+          { run: ( action: UpdateProject, state: ProjectsState) => {
+                  return this.projectsService.update(action.payload)
+                      .pipe(map((res: Project) => new ProjectUpdated(res)));
+                },
+            onError: (action: UpdateProject, error) => {
+              console.log('ERROR',error);
+            }
+          }
+      );
 
-  @Effect() delete$ = this.actions$.pipe(
-    ofType(ProjectsActionTypes.DeleteProject), //trigger event
-    switchMap( (action: DeleteProject) =>
-      this.projectsService.delete(action.payload)
-        .pipe(map((res: Project) => new ProjectDeleted(res)))//completion event
-    )
-  );
+  @Effect() delete$ =
+    this.dataPersistence
+    .pessimisticUpdate(ProjectsActionTypes.DeleteProject,
+          { run: ( action: DeleteProject, state: ProjectsState) => {
+                  return this.projectsService.delete(action.payload)
+                      .pipe(map((res: Project) => new ProjectDeleted(res)));
+                },
+            onError: (action: DeleteProject, error) => {
+              console.log('ERROR',error);
+            }
+          }
+      );
 
   constructor(
     private actions$: Actions,
+    private dataPersistence: DataPersistence<ProjectsState>,
     private projectsService: ProjectsService
   ){}
 
